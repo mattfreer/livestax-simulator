b02c05268d86d7daa3a746bd0f1c62ad6b4e568d
Khushkaran Singh Bajwa <khushkaran@users.noreply.github.com>
2015-03-31 11:37:10 +0100

    feat(toolbar): move toolbar to own panel
    Previously, the toolbar was rendered within the `panel-body`, however,
    it required the toolbar to be in a fixed position which lost required
    inheritance such as width.
    
    Now, it is rendered within its own panel inside the collapsible panel
    and sits above the `panel-body` so the body content falls under it when
    scrolling. This still allows for the panel to be hidden.
    
    As there is a second `panel-collapse`, the target for scrolling to the
    bottom for the logger needed to be updated. This has now been named
    `panel-body-container` to remain consistent with
    `panel-toolbar-container`.

--- scripts/components/lib/collapsible_panel.jsx
+++ scripts/components/lib/collapsible_panel.jsx
@@ -1,6 +1,7 @@
 "use strict";
 
 var React = require("react");
+var PanelToolbar = require("./panel_toolbar");
 
 var CollapsiblePanel = React.createClass({
   propTypes: {
@@ -17,7 +18,38 @@ var CollapsiblePanel = React.createClass({
     this.replaceState({collapsed: !this.state.collapsed});
   },
 
+  toolbarIndex(children) {
+    var toolbar = children.filter((child) => {
+      return child.type === PanelToolbar.type;
+    })[0];
+    return toolbar ? children.indexOf(toolbar) : -1;
+  },
+
+  renderableChildren(children) {
+    var toolbarIndex = this.toolbarIndex(children);
+    return children.filter((child, index) => {
+      return index !== toolbarIndex;
+    });
+  },
+
+  renderToolbar(children, target, bodyClass) {
+    var toolbarIndex = this.toolbarIndex(children);
+    if (toolbarIndex >= 0) {
+      return (
+        <div id={target} className={"panel-toolbar-container " + bodyClass}>
+          {children[toolbarIndex]}
+        </div>
+      );
+    }
+    return null;
+  },
+
+  childrenList(children) {
+    return Array.isArray(children) ? children : [children];
+  },
+
   render() {
+    var elements = this.childrenList(this.props.children);
     var type = this.props.type || "default";
     var panelClass = `panel panel-${type} panel-collapsible`;
     var headingClass = "panel-heading";
@@ -35,9 +67,10 @@ var CollapsiblePanel = React.createClass({
         <div className={headingClass} onClick={this.toggleCollapse}>
           <h4 className="panel-title"><strong>{this.props.heading}</strong></h4>
         </div>
-        <div id={this.props.target} className={bodyClass}>
+        {this.renderToolbar(elements, this.props.target, bodyClass)}
+        <div id={this.props.target} className={"panel-body-container " + bodyClass}>
           <div className="panel-body">
-            {this.props.children}
+            {this.renderableChildren(elements)}
           </div>
         </div>
       </div>
